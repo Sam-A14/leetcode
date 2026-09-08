2e0ded86354be83678efaca875d58f1b16828950
SAMRIDDHI SRIVASTAVA <samriddhisrivastava1401@gmail.com>
2026-09-08 20:21:45 +0530

    Time: 1 ms (99.92%) | Memory: 45.6 MB (19.57%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int compress(char[] chars) {
+        int i =0;
+        int write =0;
+        while(i<chars.length){
+            int j =i;
+            while(j<chars.length && chars[j]==chars[i]){
+                j++;
+            }
+            int count=j-i;
+            chars[write++]=chars[i];
+            if(count>1){
+                String str = String.valueOf(count);
+                for(char c:str.toCharArray()){
+                    chars[write++]=c;
+                }
+            }
+            i=j;
+        }
+        return write;
+    }
+}
