e43820ab91ea844bf12d94ed3b68ef887eee4772
SAMRIDDHI SRIVASTAVA <samriddhisrivastava1401@gmail.com>
2026-09-13 18:55:18 +0530

    Time: 7 ms (99.24%) | Memory: 44.6 MB (56.06%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -1,34 +1,34 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int n = img1.length;
-        List<int[]> A = new ArrayList<>();
-        List<int[]> B = new ArrayList<>();
-        for(int i =0;i<n;i++){
-            for(int j =0;j<n;j++){
-                if(img1[i][j]==1){
-                    A.add(new int[]{i,j});
+        int size = 2 * n - 1;
+
+        int[][] count = new int[size][size];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (img1[i][j] == 1) {
+
+                    for (int x = 0; x < n; x++) {
+                        for (int y = 0; y < n; y++) {
+
+                            if (img2[x][y] == 1) {
+
+                                int dr = x - i + n - 1;
+                                int dc = y - j + n - 1;
+
+                                count[dr][dc]++;
+
+                                ans = Math.max(ans, count[dr][dc]);
+                            }
+                        }
+                    }
                 }
             }
         }
-        for(int i =0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(img2[i][j]==1){
-                    B.add(new int[]{i,j});
-                }
-            }
-        }
-        HashMap <String,Integer>map=new HashMap<>();
-        int ans=0;
-        for(int[]a:A){
-            for(int[]b:B){
-                int dr=b[0]-a[0];
-                int dc=b[1]-a[1];
-                String key = dr+","+dc;
-                int count =map.getOrDefault(key,0)+1;
-                map.put(key,count);
-                ans = Math.max(ans,count);
-            }
-        }
+
         return ans;
-        }
+    }
 }
